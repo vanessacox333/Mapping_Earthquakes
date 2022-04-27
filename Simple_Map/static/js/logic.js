3e0e978dd57c754cc5c5c2b1a1971139fecd054c
Vanessa <vanessacox6@gmail.com>
2022-04-27 16:48:02 -0700

    mapping circles w/ radius based on population

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -1,14 +1,14 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
+
 // Create the map object with a center and zoom level.
 let map = L.map("mapid", {
-    center: [
-      34.0522, -118.2437
-    ],
-    zoom: 14
-  });
-
+  center: [
+    34.0522, -118.2437
+  ],
+  zoom: 14
+});
 
 //  Add a marker to the map for Los Angeles, California.
 // let marker = L.marker([34.0522, -118.2437]).addTo(map);
